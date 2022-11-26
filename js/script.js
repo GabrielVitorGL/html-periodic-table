0271242a7ce8c6d9cdfd0fd288e9e61d8b0b993a
GabrielVitorGL <gabrielvitorcom@hotmail.com>
2022-11-26 00:50:50 -0300

    zoom and glow effects

---
+++ js/script.js
@@ -0,0 +1,80 @@
+$(function () {
+    $('#label-nao_metais').hover(function () {
+        $('.nao_metais').css('box-shadow', '0 0 1px 1px #fff, 0 0 3px 3px #9c4ef5');
+    }, function () {
+        // on mouseout, reset the background colour
+        $('.nao_metais').css('box-shadow', '');
+    });
+});
+$(function () {
+    $('#label-alcalinos').hover(function () {
+        $('.alcalinos').css('box-shadow', '0 0 1px 1px #fff, 0 0 3px 3px #fe642b');
+    }, function () {
+        // on mouseout, reset the background colour
+        $('.alcalinos').css('box-shadow', '');
+    });
+});
+$(function () {
+    $('#label-semimetais').hover(function () {
+        $('.semimetais').css('box-shadow', '0 0 1px 1px #fff, 0 0 3px 3px #73ff00');
+    }, function () {
+        // on mouseout, reset the background colour
+        $('.semimetais').css('box-shadow', '');
+    });
+});
+$(function () {
+    $('#label-halogenios').hover(function () {
+        $('.halogenios').css('box-shadow', '0 0 1px 1px #fff, 0 0 3px 3px #e10d6b');
+    }, function () {
+        // on mouseout, reset the background colour
+        $('.halogenios').css('box-shadow', '');
+    });
+});
+$(function () {
+    $('#label-gases_nobres').hover(function () {
+        $('.gases_nobres').css('box-shadow', '0 0 1px 1px #fff, 0 0 3px 3px #00bbff');
+    }, function () {
+        // on mouseout, reset the background colour
+        $('.gases_nobres').css('box-shadow', '');
+    });
+});
+$(function () {
+    $('#label-lantanideos').hover(function () {
+        $('.lantanideos').css('box-shadow', '0 0 1px 1px #fff, 0 0 3px 3px #12d600');
+    }, function () {
+        // on mouseout, reset the background colour
+        $('.lantanideos').css('box-shadow', '');
+    });
+});
+$(function () {
+    $('#label-actinidios').hover(function () {
+        $('.actinidios').css('box-shadow', '0 0 1px 1px #fff, 0 0 3px 3px #ff69eb');
+    }, function () {
+        // on mouseout, reset the background colour
+        $('.actinidios').css('box-shadow', '');
+    });
+});
+$(function () {
+    $('#label-outros_metais').hover(function () {
+        $('.outros_metais').css('box-shadow', '0 0 1px 1px #fff, 0 0 3px 3px #ff30ba');
+    }, function () {
+        // on mouseout, reset the background colour
+        $('.outros_metais').css('box-shadow', '');
+    });
+});
+$(function () {
+    $('#label-transicao').hover(function () {
+        $('.transicao').css('box-shadow', '0 0 1px 1px #fff, 0 0 2px 2px #ffe51e');
+    }, function () {
+        // on mouseout, reset the background colour
+        $('.transicao').css('box-shadow', '');
+    });
+});
+$(function () {
+    $('#label-alcalinos_terrosos').hover(function () {
+        $('.alcalinos_terrosos').css('box-shadow', '0 0 1px 1px #fff, 0 0 3px 3px #4dfddd');
+    }, function () {
+        // on mouseout, reset the background colour
+        $('.alcalinos_terrosos').css('box-shadow', '');
+    });
+});
